4757c577b32bd512d0bc3153e0e4c52aac1635da
Randall Burgess <randallallenburgess@gmail.com>
2019-12-21 18:05:19 -0800

    a very unfinished application

---
+++ questions.js
@@ -0,0 +1,44 @@
+var questions = [
+    {
+        title: "Weezer's original rythm guitar player was:",
+        choices: ["Tyler Durden", "Jason Cropper", "Larry Sherbert", "Kyle Kinune"],
+        answer: "Jason Cropper"
+      },
+      {
+        title: "Who produced Weezer's first album?",
+        choices: ["Black Francis", "Butch Vig", "Ric Ocasek", "John Travolta"],
+        answer: "Ric Ocasek"
+      },
+      {
+        title: "How many albums has Weezer put out?",
+        choices: ["13", "I don't know", "3-4 good ones", "9"],
+        answer: "3-4 good ones"
+      },
+      {
+        title: "There is a holiday amongst Weezer fans, what is that holiday?",
+        choices: ["Weezer Day", "Weez Cookie Day", "Weezer T-Shirt Day", "remember the 90's day"],
+        answer: "Weezer T-shirt Day"
+      },
+      {
+        title: "Commonly used data Stypes DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+      {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+    
+  ];
+  
---
+++ script.js
@@ -0,0 +1,49 @@
+var startItUp = $('#startItUp');
+var timerHold = $('#timerHold');
+
+var totalSeconds = 0;
+
+var button1 = questions.choices[1];
+var button2 = questions.choices[2];
+var button3 = questions.choices[3];
+var button4 = questions.choices[4];
+
+var answer = questions.answer;
+
+var questionHold
+
+var scores
+
+
+
+
+
+
+function startQuiz() 
+var totalSeconds = 120;
+$('#timerHold').innerHtml('totalSeconds');
+
+function runQuiz()
+
+
+function runTheTimer() {
+
+var total_seconds = 30*1;
+var c_minutes = parseInt(total_seconds/60);
+var c_seconds = parseInt(total_seconds%60);
+
+function CheckTime(){
+document.getElementById("quiz-time-left").innerHTML
+= 'Time Left: ' + c_minutes + ' minutes ' + c_seconds + ' seconds ' ;
+
+
+}
+
+
+function init()
+
+function ()
+
+
+
+$('#startItUp').on('click', startQuiz());
